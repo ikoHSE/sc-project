a644617157033fe90ccac9b978efdab33433fbc3
iko <mail@iko.soy>
2025-12-12 10:20:06 +0300

    bump

--- src/Task.hs
+++ src/Task.hs
@@ -3,10 +3,5 @@ module Task
   )
 where
 
-import Control.Applicative
-import Control.Monad.State.Lazy
-import Data.Char
-import Data.Maybe
-
 calculate :: String -> Maybe Int
 calculate = error "TODO: calculate"
